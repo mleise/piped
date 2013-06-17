02a38bdc5ee80297cd4fc572b729e20e76e21b35
Marco Leise <marco.leise@gmx.de>
2013-06-17 17:24:49 +0200

    Replaced declaration of getpagesize() with a version from druntime.

--- src/sys/memarch.d
+++ src/sys/memarch.d
@@ -9,9 +9,10 @@ immutable ℕ allocationGranularity;
 version (linux) extern(C) {
 
 	// These are needed to create multiple views of the same physical memory pages in virtual memory on Linux
+	import core.sys.posix.sys.shm;
 	import core.sys.posix.sys.types;
-	int remap_file_pages(void *addr, ℕ size, int prot, ssize_t pgoff, int flags) nothrow;
-	void *mremap(void *old_address, ℕ old_size, ℕ new_size, int flags, ...) nothrow;
+	int remap_file_pages(void *addr, size_t size, int prot, ssize_t pgoff, int flags) nothrow;
+	void *mremap(void *old_address, size_t old_size, size_t new_size, int flags, ...) nothrow;
 	enum MREMAP_MAYMOVE = 1;
 
 }
@@ -19,12 +20,6 @@ version (linux) extern(C) {
 
 
 private:
-
-version(linux) {
-
-	extern(C) int getpagesize();
-
-}
 
 /***********************************************************************************************
  *
@@ -44,8 +39,7 @@ shared static this()
 	} else version(linux) {
 
 		// page size and allocation granularity are the same on Linux
-		import core.sys.posix.unistd;
-		allocationGranularity = systemPageSize = getpagesize();
+		allocationGranularity = systemPageSize = __getpagesize();
 
 	} else static assert(0);
 	
